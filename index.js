d97a4a24004e0d28620813b549956f533d205b54
Amiruddin Irsyad <amiruddinid@gmail.com>
2023-11-23 21:02:23 +0700

    init

--- index.js
+++ index.js
@@ -11,9 +11,32 @@ app.use('/', (req,res)=>{
   res.sendFile(__dirname+'/index.html')
 })
 
-io.on('connect', (socket)=>{
+io.on('connect', (socket) => {
+  const users = [];
   console.log('user conected')
-  socket.on('chat',(data)=>{
-    io.sockets.emit('chat',data)
+
+  socket.on('username', (data) => {
+    users.push(data);
+    io.sockets.emit('username', data)
+  })
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data)
+    io.sockets.emit('notif', 
+      `${data.user} mengirimkan pesan`)
+  })
+
+  socket.on('notif', (data) => {
+    io.sockets.emit('notification', data)
   })
 })
+
+
+
+// 
+// 1. Membuat ID unik yang memisahkan setiap client
+//    Level: Hard, keamanan terjaga
+
+// 2. membuat sebuah variable untuk membedakan 
+//    antara yang sudah submit nama dan yang belum
+//    Level: Easy, tidak ada penjagaan di server
