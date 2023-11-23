5f94b2b665de371bd1c06d38c34272dcb22c3cca
Amiruddin Irsyad <amiruddinid@gmail.com>
2023-11-23 21:52:49 +0700

    rooms

--- index.js
+++ index.js
@@ -13,22 +13,27 @@ app.use('/', (req,res)=>{
 
 io.on('connect', (socket) => {
   const users = [];
-  console.log('user conected')
 
-  socket.on('username', (data) => {
-    users.push(data);
-    io.sockets.emit('username', data)
+  socket.on('login', (data) => {
+    users.push({
+      id: socket.id,
+      room: data.room,
+      username: data.username
+    });
+    socket.join(data.room)
+    socket.in(data.room).emit('notif', `${data.username} memasuki ruangan`)
+    io.in(data.room).emit('login', {id: socket.id, ...data})
+    console.log(users);
+    io.in(data.room).emit('users', users.filter((e) => e.room === data.room))
   })
 
   socket.on('chat', (data) => {
-    io.sockets.emit('chat', data)
-    io.sockets.emit('notif', 
-      `${data.user} mengirimkan pesan`)
+    console.log(data);
+    const user = users.find((e) => e.id === socket.id)
+    io.in(user.room).emit('chat', {user: user.username, text: data.message})
+    socket.in(user.room).emit('notif', `${user.username} mengirimkan pesan`)
   })
 
-  socket.on('notif', (data) => {
-    io.sockets.emit('notification', data)
-  })
 })
 
 
